871f7351e7122951c098da9019af028be5bb8ff3
yxie@nbai.io <yxie@nbai.io>
2024-06-20 16:58:48 +0800

    Replace domain

--- src/components/spaceHardware.vue
+++ src/components/spaceHardware.vue
@@ -25,7 +25,7 @@
               <div class="title_tip flex-row pause_margin">
                 <p class="flex-row">
                   Pause Space
-                  <svg @click="system.$commonFun.goLink('https://docs.lagrangedao.org/spaces/space-settings/instance-type')" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" role="img" width="1em"
+                  <svg @click="system.$commonFun.goLink('https://docs.lagrange.computer/spaces/space-settings/instance-type')" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" role="img" width="1em"
                     height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
                     <path d="M17 22v-8h-4v2h2v6h-3v2h8v-2h-3z" fill="currentColor"></path>
                     <path d="M16 8a1.5 1.5 0 1 0 1.5 1.5A1.5 1.5 0 0 0 16 8z" fill="currentColor"></path>
